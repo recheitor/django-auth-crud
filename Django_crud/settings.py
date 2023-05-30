9093dfb1828e311d7e88b52458be38b1168b2f65
recheitor <123700084+recheitor@users.noreply.github.com>
2023-05-30 16:27:31 +0200

    default en db

--- Django_crud/settings.py
+++ Django_crud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,10 @@ WSGI_APPLICATION = 'Django_crud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite', 
+        conn_max_age=600
+    )
 }
 
 
